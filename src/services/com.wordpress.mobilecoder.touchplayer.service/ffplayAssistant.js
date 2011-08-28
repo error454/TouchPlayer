cdaea6903621bf9cd1a802ce4a7d5feaaebe0de5
Zachary Burke <zachary.burke@gmail.com>
2011-08-28 00:29:48 -0700

    Added more service call arguments, encapsulated existing arguments to prevent exploits in service calls

--- src/services/com.wordpress.mobilecoder.touchplayer.service/ffplayAssistant.js
+++ src/services/com.wordpress.mobilecoder.touchplayer.service/ffplayAssistant.js
@@ -9,10 +9,45 @@ var ffplayAssistant = function(){}
 
 /*
 *	service call: 	play
-*	function: 		plays a video using ffplay with the specified arguments
+*	function: 		plays a video using ffplay with the specified options
+*   argument list:
+*		source: the absolute path of the file to play
+* 		visualization: waves, video(default) or rdft
+*		audio: boolean
+*		flipVertical: boolean
+*		threads: integer
+*		subIndex: integer
+* 		
 */
 ffplayAssistant.prototype.run = function(future){
 
-	var cmd = new CommandLine("./ffplay " + this.controller.args.switches + " \"" + this.controller.args.source + "\"", future);
+	//Build up ffplay command
+	var inArgs = this.controller.args;
+	var args = "";
+	
+	if(inArgs.audio == false){
+		args += "-an ";
+	}
+	
+	if(inArgs.threads > 0){
+		args += "-threads " + inArgs.threads + " ";
+	}
+	
+	if(inArgs.visualization == "waves"){
+		args += "-showmode 1 ";
+	}
+	else if(inArgs.visualization == "rdft"){
+		args += "-showmode 2 ";
+	}
+	
+	if(inArgs.flipVertical == true){
+		args += "-vf \"vflip\" ";
+	}
+	
+	if(inArgs.subIndex > 0){
+		args += "-sst " + inArgs.subIndex + " ";
+	}
+				
+	var cmd = new CommandLine("./ffplay " + args + " \"" + this.controller.args.source + "\"", future);
 	cmd.run();
 }
